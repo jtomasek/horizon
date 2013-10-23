d9b2856451b179dd5567a8e9517359eb889e9b41
Jordan OMara <jomara@redhat.com>
2013-10-23 18:43:20 -0400

    Getting angular directives loaded

--- horizon/static/horizon/js/horizon.membership.js
+++ horizon/static/horizon/js/horizon.membership.js
@@ -423,6 +423,90 @@ horizon.membership = {
             }
       });
     });
+
+
+  },
+
+  init_angular: function () {
+    angular.module('horizonApp', []);
+    console.log("init horizon module");
+
+    //compiled_temp = $compile('membership_angular.html')($scope);
+    //angular.element("#membership_div").html(compiled_temp);
+
+    angular.module('horizonApp').directive('membership',
+    [
+        function() {
+            return {
+                restrict: 'E',
+                replace: true,
+                scope :{ status: '='},
+                //template: '<div></div>',
+                templateUrl: 'membership_angular.html',
+                controller: function() {
+                     console.log("directive loaded");
+
+                }
+
+            };
+        }]);
+
+    angular.module('horizonApp').filter('searchFor', function () {
+        return function (arr, searchString) {
+            if (!searchString) {
+                return arr;
+            }
+            var result = [];
+            angular.forEach(arr, function (item) {
+                if (item.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
+                    result.push(item);
+                }
+            });
+            return result;
+        };
+    });
+
+    angular.module('horizonApp').controller('MembershipController',
+        ['$scope',
+        function($scope) {
+
+            console.log("Loaded angular membership controller");
+            $scope.chainsaw = "TEXAS";
+            $scope.current_membership = [];
+            $scope.data = [],
+            $scope.roles = [],
+            $scope.has_roles = [],
+            $scope.default_role_id = [],
+
+            $scope.open = function() {
+                console.log("opened");
+            }
+
+            $scope.submit = function() {
+                $http.post('/admin/domains/default/update/', {
+                    subject: $scope.subject
+                }).success(function(out_data) {
+                    // do something
+                });
+            };
+
+            $scope.addToDomain = function (group) {
+                $scope.domainGroups.push(group);
+                var index = $scope.allGroups.indexOf(group);
+                $scope.allGroups.splice(index, 1);
+            };
+
+            $scope.removeFromDomain = function (group) {
+                var index = $scope.domainGroups.indexOf(group);
+                $scope.domainGroups.splice(index, 1);
+                $scope.allGroups.push(group);
+            };
+
+    }]);
+
+    //angular.element(document).ready(function() {
+    //   angular.bootstrap(document, ['horizonApp']);
+    //});
   },
 
   /*
@@ -430,7 +514,6 @@ horizon.membership = {
    **/
   workflow_init: function(modal, step_slug, step_id) {
     console.log("Loaded jquery workflow init");
-
 
     // fix the dropdown menu overflow issues
     $(".tab-content, .workflow").addClass("dropdown_fix");
@@ -483,6 +566,11 @@ horizon.membership = {
           $(filter + ' .btn-group:even').addClass('dark_stripe');
           $(filter + ' .btn-group:last').addClass('last_stripe');
         });
+
+
     });
+
+    horizon.membership.init_angular();
+    angular.bootstrap(document, ['horizonApp']);
   }
 };
--- horizon/static/horizon/js/horizon.membership_angular.js
+++ horizon/static/horizon/js/horizon.membership_angular.js
@@ -1,79 +1,78 @@
-angular.module('horizonApp', []);
-console.log("bootstrap horizonApp");
-
-angular.module('horizonApp').directive('membership',
-[
-    function() {
-        return {
-            restrict: 'E',
-            replace: true,
-            scope :{ status: '='},
-            template: '<div></div>',
-            templateUrl: 'membership_angular.html',
-            controller: function() {
-                 console.log("directive loaded");
-
-            }
-
-        };
-    }]);
-
-angular.module('horizonApp').filter('searchFor', function () {
-    return function (arr, searchString) {
-        if (!searchString) {
-            return arr;
-        }
-        var result = [];
-        angular.forEach(arr, function (item) {
-            if (item.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
-                result.push(item);
-            }
-        });
-        return result;
-    };
-});
-
-angular.module('horizonApp').controller('MembershipController',
-    ['$scope',
-    function($scope) {
-        console.log("Loaded angular membership controller");
-        $scope.chainsaw = "TEXAS";
-        $scope.current_membership = [];
-        $scope.data = [],
-        $scope.roles = [],
-        $scope.has_roles = [],
-        $scope.default_role_id = [],
-
-        item = $scope.allGroups[0]
-        $scope.open = function() {
-            console.log("opened");
-        }
-
-        $scope.submit = function() {
-            $http.post('/admin/domains/default/update/', {
-                subject: $scope.subject
-            }).success(function(out_data) {
-                // do something
-            });
-        };
-
-        $scope.addToDomain = function (group) {
-            $scope.domainGroups.push(group);
-            var index = $scope.allGroups.indexOf(group);
-            $scope.allGroups.splice(index, 1);
-        };
-
-        $scope.removeFromDomain = function (group) {
-            var index = $scope.domainGroups.indexOf(group);
-            $scope.domainGroups.splice(index, 1);
-            $scope.allGroups.push(group);
-        };
-
-}]);
-
-angular.element(document).ready(function() {
-   angular.bootstrap(document, ['horizonApp']);
-   debugger;
-});
+//angular.module('horizonApp', []);
+//console.log("bootstrap horizonApp");
+//
+//angular.module('horizonApp').directive('membership',
+//[
+//    function() {
+//        return {
+//            restrict: 'E',
+//            replace: true,
+//            scope :{ status: '='},
+//            template: '<div></div>',
+//            templateUrl: 'membership_angular.html',
+//            controller: function() {
+//                 console.log("directive loaded");
+//
+//            }
+//
+//        };
+//    }]);
+//
+//angular.module('horizonApp').filter('searchFor', function () {
+//    return function (arr, searchString) {
+//        if (!searchString) {
+//            return arr;
+//        }
+//        var result = [];
+//        angular.forEach(arr, function (item) {
+//            if (item.toLowerCase().indexOf(searchString.toLowerCase()) !== -1) {
+//                result.push(item);
+//            }
+//        });
+//        return result;
+//    };
+//});
+//
+//angular.module('horizonApp').controller('MembershipController',
+//    ['$scope',
+//    function($scope) {
+//        console.log("Loaded angular membership controller");
+//        $scope.chainsaw = "TEXAS";
+//        $scope.current_membership = [];
+//        $scope.data = [],
+//        $scope.roles = [],
+//        $scope.has_roles = [],
+//        $scope.default_role_id = [],
+//
+//        item = $scope.allGroups[0]
+//        $scope.open = function() {
+//            console.log("opened");
+//        }
+//
+//        $scope.submit = function() {
+//            $http.post('/admin/domains/default/update/', {
+//                subject: $scope.subject
+//            }).success(function(out_data) {
+//                // do something
+//            });
+//        };
+//
+//        $scope.addToDomain = function (group) {
+//            $scope.domainGroups.push(group);
+//            var index = $scope.allGroups.indexOf(group);
+//            $scope.allGroups.splice(index, 1);
+//        };
+//
+//        $scope.removeFromDomain = function (group) {
+//            var index = $scope.domainGroups.indexOf(group);
+//            $scope.domainGroups.splice(index, 1);
+//            $scope.allGroups.push(group);
+//        };
+//
+//}]);
+//
+//angular.element(document).ready(function() {
+//   angular.bootstrap(document, ['horizonApp']);
+//});
 
 
